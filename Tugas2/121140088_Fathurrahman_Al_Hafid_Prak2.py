4b6292df12ec6773f2eaadb0f98f0cf30d48ad96
FathurRA4 <126642970+FathurRA4@users.noreply.github.com>
2023-03-04 20:55:52 +0700

    Add files via upload

---
+++ Tugas2/121140088_Fathurrahman_Al_Hafid_Prak2.py
@@ -0,0 +1,24 @@
+class Mahasiswa:
+    def __init__(self, Nama, NIM, Kelas_PBO, Jumlah_SKS):
+        self.nama = Nama
+        self.NIM = NIM
+        self.Kelas_PBO = Kelas_PBO
+        self.Jumlah_SKS = Jumlah_SKS
+
+    def cetakdata(self):
+        print("Data mahasiswa: ")
+        print()
+        print("Nama:", self.nama)
+        print()
+        print("NIM:", self.NIM)
+        print()
+        print("Kelas PBO:", self.Kelas_PBO)
+        print()
+        print("Jumlah SKS yang diambil:", self.Jumlah_SKS)
+
+
+Data_Mahasiswa = Mahasiswa("Fathurrahman Al Hafid", 121140088, "RB", 24)
+
+Data_Mahasiswa.cetakdata()
+
+# Buat akun github, buat repository Prak-PBO,folder tugas1, isi = NIM_nama_tugas1.py
